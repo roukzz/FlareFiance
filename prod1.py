4e6c7a728fb798a25e170be3175cfcc879a9f245
roukzz <arabfarouk@hotmail.com>
2022-01-22 13:17:23 -0500

    nests added: dynamic number of buttons is handeled

--- prod1.py
+++ prod1.py
@@ -1,3 +1,4 @@
+from asyncio.windows_events import NULL
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
@@ -11,13 +12,12 @@ from pynput.keyboard import Key, Controller
 PATH ='C:\Program Files (x86)\chromedriver.exe'
 EXTENTION_PATH='C:\metamask_ext\metamask.crx'
 confirm_xpath = '//*[@id="app-content"]/div/div[3]/div/div[4]/div[3]/footer/button[2]'
+nests_xpath= '//*[@id="root"]/div[3]/div/div/div/div[2]/div/div[3]/div/div[5]/p'
+nest=""
 
 opt = Options()
 opt.add_experimental_option("debuggerAddress","localhost:8989")
 driver = webdriver.Chrome(PATH,chrome_options=opt)
-# driver.get("chrome-extension://nkbihfbeogaeaoehlefnkodbefgpgknn/home.html#")
-# print('metamask Launched')
-# confirm button : //*[@id="app-content"]/div/div[2]/div/div[4]/div[3]/footer/button[2]
 print('launch flare finance')
 driver.get("https://xfl.flr.finance/wsgb/liquidations")
 pId= driver.current_window_handle
@@ -31,42 +31,69 @@ print("child ID: ", cId)
 driver.switch_to.window(pId)
 while True:
     driver.refresh()
-    path='//*[@id="root"]/div[3]/div[1]/div[1]/div[3]/div[2]/div[1]/div[2]/div[1]/div[1]'
+    
+    try:
+        # nests_present=EC.presence_of_element_located((By.XPATH, nests_xpath))
+        # WebDriverWait(driver, 5).until(nests_present)
+        time.sleep(3)
+        nests = driver.find_element_by_xpath(nests_xpath).text
+        print("text nests :",nests)
+        nests_integer= int(nests)
+        print("integer nest: ",nests_integer)
+       
+    except Exception as exception:
+        print(exception)
+        print("could not find nests")
 
-    for x in range(7,31):
-        i = str(x)
-        xpath = '//*[@id="root"]/div[3]/div/div/div/div[2]/div/div[1]/div/div/div[2]/div['+i+']/div[4]/button'
-        # print(xpath)
-        try : 
-            element_present = EC.presence_of_element_located((By.XPATH, xpath))
-            WebDriverWait(driver, 40).until(element_present)
-            driver.find_element_by_xpath(xpath).click()
-            print("button : ", x, " clicked ", " TIME: ",datetime.now())
-            if (x%6) == 0:
-                try:
-                    # metamask
-                    driver.switch_to.window(cId[1])
-                    print("metamask window: ", x)
-                    confirm_element_present = EC.presence_of_element_located((By.XPATH, confirm_xpath))
-                    WebDriverWait(driver, 0.5).until(confirm_element_present)
-                    if (confirm_element_present) :
-                        driver.find_element_by_xpath('//*[@id="app-content"]/div/div[3]/div/div[4]/div[3]/footer/button[2]').click()
-                        print("**********************************")
-                        print("confirm button clicked")
-                        print("TIME: ", datetime.now())
-                        print("**********************************")
-                        time.sleep(20)
-                        print("waiting for 20 sec") 
-                    driver.refresh()
-                    driver.switch_to.window(pId)
-                except Exception as exception:
-                    driver.refresh()
-                    driver.switch_to.window(pId)
-                    print("confirm button not found")
+    
+    
+    
+    if (nests) :    
+        for x in range(5,(nests_integer+1)):
+            i = str(x)
+            
+            liquidate_xpath = '//*[@id="root"]/div[3]/div/div/div/div[2]/div/div[1]/div/div/div[2]/div['+i+']/div[4]/button'
+            # print(liquidate_xpath)
+            try : 
+                # wait to find the liquidate buttons
+                element_present = EC.presence_of_element_located((By.XPATH, liquidate_xpath))
+                WebDriverWait(driver, 40).until(element_present)
+                driver.find_element_by_xpath(liquidate_xpath).click()
+                print("button : ", x, " clicked ", " TIME: ",datetime.now())
+                if (x%nests_integer) == 0:
+                    try:
+                        # metamask
+                        driver.switch_to.window(cId[1])
+                        print("metamask window: ", x)
+                        try:
+                            confirm_element_present = EC.presence_of_element_located((By.XPATH, confirm_xpath))
+                            WebDriverWait(driver,0.2).until(confirm_element_present)
+                        except Exception as exception:
+                            # print(exception)
+                            print("CONFIRM button not found at first sight, refresh MetaMask wallet")
+                            driver.refresh()
+                        
+                        confirm_element_present = EC.presence_of_element_located((By.XPATH, confirm_xpath))
+                        WebDriverWait(driver, 2).until(confirm_element_present)
+                        
+                        if (confirm_element_present) :
+                            driver.find_element_by_xpath(confirm_xpath).click()
+                            print("**********************************")
+                            print("confirm button clicked")
+                            print("TIME: ", datetime.now())
+                            print("**********************************")
+                            time.sleep(20)
+                            print("waiting for 20 sec") 
+                        driver.refresh()
+                        driver.switch_to.window(pId)
+                    except Exception as exception:
+                        driver.refresh()
+                        driver.switch_to.window(pId)
+                        print("confirm button not found")
 
-        except Exception as exception:
-            driver.refresh()
-            print("counld not find element")
+            except Exception as exception:
+                driver.refresh()
+                print("counld not find liquidate elements")
 
 
 
